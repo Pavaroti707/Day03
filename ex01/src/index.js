16a16b7de8802a680759d965cbabdb34702b9670
Pavaroti707 <hasicmahir@gmail.com>
2021-03-11 01:43:16 +0100

    My first mini project

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+// Only chancge code below this line
+console.log('Hello World') 
+// Only chancge code above this line
+}   
+main() ;
+module.exports = main;
---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+
+    // Only change coode below this line
+     // Dodajem komentar
+     /*
+     jos jedan komentar
+     */
+    // Only change code above this line
+}
+
+main();
+module.export = main;
